31551ac89c734e10f413bbdf1bfe9df90b52da82
Lucas Gama <lucasgdon@gmail.com>
2024-11-21 22:15:06 -0300

    Adicionada a conexão ao banco de dados, e listagem dos posts deste banco Mongo DB

--- server.js
+++ server.js
@@ -1,22 +1,7 @@
 import express from 'express';
+import conectarAoBanco from './src/config/dbConfig.js';
 
-const posts = [
-    {
-        id: 1,
-        descricao: 'Foto teste',
-        imagem: "https//placecats.com/millie/300/150"
-    },
-    {
-        id: 2,
-        descricao: 'Foto teste 2',
-        imagem: "https//placecats.com/millie/300/150"
-    },
-    {
-        id: 3,
-        descricao: 'Foto teste 3',
-        imagem: "https//placecats.com/millie/300/150"
-    },
-]
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
 
 const app = express();
 // Retorna JSON caso seja aplicável
@@ -26,18 +11,25 @@ app.listen(3000, () => {
     console.log('Servidor escutando...');
 });
 
+async function getPosts(){
+    const db = conexao.db("imersao-backend");
+    const colecao = db.collection("posts");
+    return colecao.find().toArray();
+}
+
 function buscarPostPorID(id){
     return posts.findIndex((post) => {
         return post.id === Number(id);
     });
 }
 
-app.get('/posts', (req, res) => {
-    res.status(200).json(posts)
+app.get('/posts', async (req, res) => {
+    const posts = await getPosts();
+    res.status(200).json(posts);
 });
 
-app.get('/posts/:id', (req, res) => {
-    const index = buscarPostPorID(req.params.id);
-    res.status(200).json(posts[index]);
-});
+// app.get('/posts/:id', (req, res) => {
+//     const index = buscarPostPorID(req.params.id);
+//     res.status(200).json(posts[index]);
+// });
 
