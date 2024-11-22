5b6f6d2a1383a7fe9ad1e46b75e602a690dfebb0
Lucas Gama <lucasgdon@gmail.com>
2024-11-21 21:31:27 -0300

    Adicionado endpoints para retornar todos os posts (array em memória) e  para buscar post por ID

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 import express from 'express';
 
+const posts = [
+    {
+        id: 1,
+        descricao: 'Foto teste',
+        imagem: "https//placecats.com/millie/300/150"
+    },
+    {
+        id: 2,
+        descricao: 'Foto teste 2',
+        imagem: "https//placecats.com/millie/300/150"
+    },
+    {
+        id: 3,
+        descricao: 'Foto teste 3',
+        imagem: "https//placecats.com/millie/300/150"
+    },
+]
+
 const app = express();
+// Retorna JSON caso seja aplicável
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Servidor escutando...');
 });
 
-app.get('/api', (req, res) => {
-    
+function buscarPostPorID(id){
+    return posts.findIndex((post) => {
+        return post.id === Number(id);
+    });
+}
+
+app.get('/posts', (req, res) => {
+    res.status(200).json(posts)
 });
+
+app.get('/posts/:id', (req, res) => {
+    const index = buscarPostPorID(req.params.id);
+    res.status(200).json(posts[index]);
+});
+
